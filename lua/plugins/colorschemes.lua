73a223c2343c6cfd8958af750767eaaa8293256b
InnocentStranger <aryancoder440@gmail.com>
2025-05-22 16:55:37 +0000

    updated colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -14,4 +14,32 @@ return {
 	{
 		"rose-pine/neovim",
 	},
+
+	{
+		"atmosuwiryo/vim-winteriscoming",
+	},
+
+	{
+		"niyabits/calvera-dark.nvim",
+	},
+
+	{
+		"fcancelinha/nordern.nvim",
+	},
+
+	{
+		"Yazeed1s/minimal.nvim",
+	},
+
+	{
+		"cpea2506/one_monokai.nvim",
+	},
+
+	{
+		"lourenci/github-colors",
+	},
+
+	{
+		"sainnhe/sonokai",
+	},
 }
