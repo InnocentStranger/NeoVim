46940b9e786610e526f42626ae25ecc732de0161
aryan.rajput <aryan.rajput@nucleussoftware.com>
2025-05-27 23:41:36 +0530

    updated conform to add enable/disable commands

--- lua/plugins/formatter/conform.lua
+++ lua/plugins/formatter/conform.lua
@@ -23,19 +23,39 @@ return {
 			default_format_opts = {
 				lsp_format = "fallback",
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
+			format_on_save = function(bufnr)
+				if vim.g.conform_disable then
+					return
+				end
+				return {
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				}
+			end,
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>fmt", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
+			if not vim.g.conform_disable then
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				})
+			else
+				vim.notify("Conform formatting is currently disabled.", vim.log.levels.WARN)
+			end
 		end, { desc = "Format file or range (in visual mode)" })
+
+		-- Commands to enable/disable conform formatting
+		vim.api.nvim_create_user_command("ConformEnable", function()
+			vim.g.conform_disable = false
+			vim.notify("Conform formatting enabled", vim.log.levels.INFO)
+		end, {})
+
+		vim.api.nvim_create_user_command("ConformDisable", function()
+			vim.g.conform_disable = true
+			vim.notify("Conform formatting disabled", vim.log.levels.INFO)
+		end, {})
 	end,
 }
